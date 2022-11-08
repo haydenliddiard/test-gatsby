cb29d6e11bb147ce059f0a7a356c527586e980c0
Hayden Liddiard <cid196420@gmail.com>
2022-11-08 13:38:22 -0800

    added multi versions of icon to images and registered them in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,13 +32,62 @@ module.exports = {
   {
     resolve: `gatsby-plugin-manifest`,
     options: {
-      name: `testcoffeeplace`,
+      name: `test-coffee-place`,
       short_name: `testcoffeeplace`,
       start_url: `/`,
       background_color: `#f7f0eb`,
       theme_color: `#a2466c`,
       display: `minimal-ui`,
-      icon: `src/images/favicon.svg`,
+      icons: [
+        {
+          src: "src/images/icon-48x48.png",
+          sizes: "48x48",
+          type: "image/png",
+          purpose: "maskable"
+        },
+        {
+          src: "src/images/icon-72x72.png",
+          sizes: "72x72",
+          type: "image/png",
+          purpose: "maskable"
+        },
+        {
+          src: "src/images/icon-96x96.png",
+          sizes: "96x96",
+          type: "image/png",
+          purpose: "maskable"
+        },
+        {
+          src: "src/images/icon-144x144.png",
+          sizes: "144x144",
+          type: "image/png",
+          purpose: "maskable"
+        },
+        {
+          src: "src/images/icon-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable"
+        },
+        {
+          src: "src/images/icon-256x256.png",
+          sizes: "256x256",
+          type: "image/png",
+          purpose: "maskable"
+        },
+        {
+          src: "src/images/icon-384x384.png",
+          sizes: "384x384",
+          type: "image/png",
+          purpose: "maskable"
+        },
+        {
+          src: "src/images/icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable"
+        },
+      ]
     },
   },
 ]
